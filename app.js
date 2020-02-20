8b801b0ceb53f5c253caf300458daaada6affca1
dc <chendana95@gmail.com>
2020-02-20 03:19:48 -0500

    added viewEmployee function that returns all employees

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const connection = mysql.createConnection({
     database: "cms_db"
 });
 
+//create connection and call the first inquirer function
 connection.connect((err) => {
     if (err) throw err;
     runSearch();
@@ -20,12 +21,62 @@ const runSearch = () => {
             name: "action",
             type: "rawlist",
             message: "What would you like to do?",
-            choices: [""]
+            choices: ["View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager"
+            ]
         })
         .then((res) => {
             switch (res.action) {
-                case "":
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "View All Employees By Department":
+                    viewByDepartment();
+                    break;
+                case "View All Employees By Manager":
+                    viewByManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateRole();
+                    break;
+                case "Update Employee Manager":
+                    updateManager();
                     break;
             }
+        })
+}
+
+//view all employees... join employee and employee role tables
+const viewEmployees = () => {
+    const query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        for (let i = 0; i < res.length; i++) {
+            console.log(`${res[i].id}. ${res[i].first_name} ${res[i].last_name} | Role: Manager`);
+        }
+        runSearch();
+    })
+}
+
+
+const viewByDepartment = () => {
+    inquirer
+        .prompt({
+            name: "",
+            type: "",
+            message: "",
+            choices: []
+        }).then((res) => {
+
         })
 }
