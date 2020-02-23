38f4c2791e0709adce4d2c454d6a105008769867
dc <chendana95@gmail.com>
2020-02-23 00:10:56 -0500

    added view departments function

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
+
+// const departments = ["Sales", "Engineering", "Finance", "Legal"];
+// const roles = ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"];
+
+const departments = []
+const roles = [];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -22,6 +29,8 @@ const runSearch = () => {
             type: "rawlist",
             message: "What would you like to do?",
             choices: ["View All Employees",
+                "View All Departments",
+                "View All Positions",
                 "View All Employees By Department",
                 "View All Employees By Manager",
                 "Add Employee",
@@ -34,6 +43,12 @@ const runSearch = () => {
             switch (res.action) {
                 case "View All Employees":
                     viewEmployees();
+                    break;
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+                case "View All Positions":
+                    viewPositions();
                     break;
                 case "View All Employees By Department":
                     viewByDepartment();
@@ -58,6 +73,7 @@ const runSearch = () => {
 }
 
 //view all employees... join employee and employee role tables
+//id, first, last, title, department, salary, manager
 const viewEmployees = () => {
     const query = "SELECT * FROM employee";
     connection.query(query, (err, res) => {
@@ -68,14 +84,27 @@ const viewEmployees = () => {
     })
 }
 
+const viewDepartments = () => {
+    const query = "SELECT * FROM department";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+    runSearch();
+}
 
+const viewPositions = () => {
+
+}
+
+//view by department, shows only first name, last name, and role 
 const viewByDepartment = () => {
     inquirer
         .prompt({
             name: "department",
             type: "rawlist",
             message: "Which department would you like to view?",
-            choices: ["Sales", "Engineering", "Finance", "Legal"]
+            choices: departments
         }).then((res) => {
             const query = "SELECT employee.first_name, employee.last_name, employee_role.title FROM employee_role JOIN employee ON employee_role.id = employee.role_id WHERE employee_role.title = ?;"
             connection.query(query, [res.department], (err, res) => {
@@ -85,4 +114,94 @@ const viewByDepartment = () => {
             });
             runSearch();
         });
+}
+
+//bonus
+const viewByManager = () => {
+
+}
+
+//
+const addEmployee = () => {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "first_name",
+            message: "What is the employee's first name?"
+        }, {
+            type: "input",
+            name: "last_name",
+            message: "What is the employee's last name?"
+        }, {
+            type: "rawlist",
+            name: "title",
+            message: "What is the employee's role title?",
+            choices: roles
+        }, {
+            type: "input",
+            name: "last_name",
+            message: "Who is the employee's manager?",
+            choices: ["", "None"]
+        }).then(res => {
+            const query = ""
+        });
+}
+
+//bonus
+const removeEmployee = () => {
+    inquirer.prompt({
+        type: "input",
+        name: "delete",
+        message: "Which employee would you like to remove?"
+    }).then(res => {
+
+    });
+}
+
+const updateRole = () => {
+    inquirer.prompt({
+        type: "input",
+        name: "id",
+        message: "Which employee's role would you like to update by ID?"
+    }, {
+        type: "rawlist",
+        name: "update",
+        message: "What would you like to update their role to?",
+        choices: roles
+    }).then(res => {
+        let roleIndex;
+        switch (res.update) {
+            case "Sales Lead":
+                roleIndex = 0;
+                break;
+            case "Salesperson":
+                roleIndex = 1;
+                break;
+            case "Lead Engineer":
+                roleIndex = 2;
+                break;
+            case "Software Engineer":
+                roleIndex = 3;
+                break;
+            case "Account Manager":
+                roleIndex = 4;
+                break;
+            case "Accountant":
+                roleIndex = 5;
+                break;
+            case "Legal Team Lead":
+                roleIndex = 6;
+                break;
+        }
+        const query = "UPDATE employee SET ? WHERE ?;"
+        connection.query(query, [roleIndex, res.id], (err, res) => {
+            console.log(`The employee of ID ${res.id}'s role has been updated to ${res.update}`)
+        });
+        runSearch();
+    });
+}
+
+//bonus
+const updateManager = () => {
+
 }
